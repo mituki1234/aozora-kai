c43d3b1f1933b29f61c4b4ea6147280edccdaa42
abkd_ <144220463+mituki1234@users.noreply.github.com>
2025-04-15 12:44:53 +0900

    Update route.ts
    ue

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,3 +1,4 @@
+'use server'
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
@@ -6,17 +7,12 @@ export async function GET(request: NextRequest) {
         const books = searchParams.get('books');
         const limit = searchParams.get('limit');
         const url = `https://api.aozora.gr.jp/v1/search?books=${books}&limit=${limit}`;
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-        });
-        if (!response.ok) {
-            return NextResponse.json({ error: 'Failed to fetch data from Aozora API' }, { status: 500 });
-        }
-        const data = await response.json();
-        return NextResponse.json(data);
+        axios.get(url)
+            .then((responce) => {
+                return NextResponse.json(responce.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
     }
 }
